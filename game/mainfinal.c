9f4a2c5fb14d84ecbe9999d019a8b0619fd2d603
yesminezaghdene03 <80226658+yesminezaghdene03@users.noreply.github.com>
2021-05-02 04:44:11 +0100

    Update mainfinal.c

--- game/mainfinal.c
+++ game/mainfinal.c
@@ -221,3 +221,166 @@ int main()
    SDL_Quit();
   return 0;
 }
+///////////////////////////////main.c yesmin/////////////////////////////////////////////
+int main()
+{
+    SDL_Surface *screen,*image;
+    mouvement M;
+    background b;
+    personne p ;
+    int continuer = 1;  
+int keysHeld[325]={0};  
+    Mix_Music *music;
+
+
+////////////////////////////////////// musique continue/////////////////////////
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024);
+	music = Mix_LoadMUS("music.mp3") ;
+	Mix_PlayMusic(music,-1);
+//////////////////////////////
+
+
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);//// initialisation de SDL 
+    SDL_WM_SetIcon(IMG_Load("icon.png"), NULL);/// image de l'icon
+    screen = SDL_SetVideoMode(1200, 1000, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);//// creation de fenetre 
+    SDL_WM_SetCaption("volta", NULL); /// nom de fenetre 
+
+
+    init_Mouvement(&M); // appel fonction initialisation des deplacement
+    initBack(&b);// appel fonction initialisation de background 1 et 2
+init_perso(&p);
+
+
+
+///////////////////////////////////////////////// evenement////////////////////////////////////////
+    SDL_Event event;
+    SDL_EnableKeyRepeat(1, 0);//curseur continue
+    while (continuer)
+
+    {
+aficherBack(screen,&b);
+afficher_perso(&p,screen);
+
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+
+                continuer = 0;
+                break;
+
+            case SDL_KEYDOWN:
+keysHeld[event.key.keysym.sym]=1;
+
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_ESCAPE:
+                    continuer = 0;
+                    break;
+
+                case SDLK_RIGHT:
+                    M.right = 1;
+                    break;
+
+                case SDLK_LEFT:
+                    M.left = 1;
+                    break;
+
+                case SDLK_d:
+                    M.d = 1;
+                    break;
+
+                case SDLK_q:
+                    M.q = 1;
+                    break;
+
+                case SDLK_DOWN:
+                    M.down = 1;
+                    break;
+
+                case SDLK_UP:
+                    M.up = 1;
+                    break;
+
+                case SDLK_z:
+                    M.z = 1;
+                    break;
+
+                case SDLK_s:
+                    M.s = 1;
+                    break;
+                }
+                break;
+
+            case SDL_KEYUP:
+keysHeld[event.key.keysym.sym]=0;
+                switch (event.key.keysym.sym)
+                {
+
+                case SDLK_RIGHT:
+                    M.right = 0;
+                    break;
+
+                case SDLK_LEFT:
+                    M.left = 0;
+                    break;
+
+                case SDLK_d:
+                    M.d = 0;
+                    break;
+
+                case SDLK_q:
+                    M.q = 0;
+                    break;
+
+                case SDLK_DOWN:
+                    M.down = 0;
+                    break;
+
+                case SDLK_UP:
+                    M.up = 0;
+                    break;
+
+                case SDLK_z:
+                    M.z = 0;
+                    break;
+
+                case SDLK_s:
+                    M.s = 0;
+                    break;
+
+                }
+                break;
+            }
+//////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////// appele fonction scrolling ////////////////////////// 
+                scrolling_right(&b, 4, screen, M);
+		scrolling_right_partage(&b, 4,screen,M);
+                scrolling_left(&b, 4, screen, M);
+		scrolling_left_partage(&b, 4,screen,M);
+		scrolling_up(&b, 4,screen,M);
+		scrolling_up_partage(&b, 4,screen,M);
+		scrolling_down(&b, 4,screen,M);
+		scrolling_down_partage(&b, 4,screen,M);
+////////////////////
+
+
+        }
+
+	aficherBack_partage(screen,&b);/// appel fonction affichage back 2 avec partage d'ecran
+        SDL_Flip(screen);// mise a jour de l'ecran
+
+    }
+
+    SDL_FreeSurface(screen);
+    SDL_FreeSurface(image);
+    Mix_FreeMusic(music); 
+    SDL_Quit();
+    return 0;
+}
